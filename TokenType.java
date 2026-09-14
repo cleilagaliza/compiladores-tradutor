ed1b2d1a2165fd1c69eff3797114601304514a74
Cleila Galiza <57466359+cleilagaliza@users.noreply.github.com>
2026-09-13 23:49:08 -0300

    implementa modelo de tokens e scanner

---
+++ TokenType.java
@@ -0,0 +1,13 @@
+public enum TokenType {
+    PLUS,       // +
+    MINUS,      // -
+    STAR,       // *
+    SLASH,      // /
+    NUMBER,     // Números de múltiplos dígitos
+    IDENT,      // Identificadores (nomes de variáveis)
+    LET,        // Palavra reservada 'let'
+    PRINT,      // Palavra reservada 'print'
+    EQ,         // =
+    SEMICOLON,  // ;
+    EOF         // Fim de arquivo
+}
